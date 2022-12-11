b8974d20b7ab62198f6255b2d6c2852dfad33d35
Christian Smith <christian.46.smith@gmail.com>
2022-12-11 11:30:17 -0500

    Added queries and mutations to typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,4 +23,15 @@ const typeDefs = gql`
         user: User
     }
 
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
+    }
+
 `
